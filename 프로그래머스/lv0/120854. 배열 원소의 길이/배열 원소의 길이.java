cbc4342ebb19fbddf11f40afd9f14ea34a93c985
Shinny <79249838+siniya979@users.noreply.github.com>
2023-01-28 05:12:52 +0900

    Update 배열 원소의 길이.java

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,9 +1,13 @@
 class Solution {
     public int[] solution(String[] strlist) {
-        int[] answer = new int[strlist.length];
+        return Arrays.stream(strlist).mapToInt(String::length).toArray();
+    }
+}
+
+/*
+ int[] answer = new int[strlist.length];
         for(int i = 0; i < strlist.length; i++){
             answer[i] = strlist[i].length();
         }
         return answer;
-    }
-}
+*/
