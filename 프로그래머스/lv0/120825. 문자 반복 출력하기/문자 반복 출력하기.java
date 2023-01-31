908d2335565cd14b751e3bf5b263f588da9f6e68
Shinny <79249838+siniya979@users.noreply.github.com>
2023-01-31 14:22:46 +0900

    Update 문자 반복 출력하기.java

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -5,17 +5,12 @@ class Solution {
     public String solution(String my_string, int n) {
         
         String answer = "";
-        List<String> str = new ArrayList<>();
-        
-        for(int i = 0; i < my_string.length(); i++) {
-            for(int j = 0; j<n; j++) {
-                str.add(String.valueOf(my_string.charAt(i)));
+
+        for(int i=0; i<my_string.length(); i++){
+            for(int j=0; j<n; j++){
+                answer+=my_string.charAt(i);
             }
-        }
-        
-        for(String a : str) {
-            answer += a;
-        }
+        }       
         
         return answer;
     }
